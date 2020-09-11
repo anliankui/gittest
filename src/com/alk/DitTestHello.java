c0132ebe90a450deb2f8c6359409603cd76c3525
alk <1745298681@qq.com>
2020-09-11 11:25:40 +0800

    添加gittest

---
+++ src/com/alk/DitTestHello.java
@@ -0,0 +1,14 @@
+package com.alk;
+
+/**
+ * @program: gittest
+ * @description:
+ * @author: alk
+ * @create: 2020-09-11 11:19:56
+ **/
+
+public class DitTestHello {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
---
+++ src/com/alk/Test01.java
@@ -0,0 +1,14 @@
+package com.alk;
+
+/**
+ * @program: gittest
+ * @description:
+ * @author: alk
+ * @create: 2020-09-11 11:23:53
+ **/
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
